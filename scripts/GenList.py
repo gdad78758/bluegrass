28443607f16f625e1399b81c4913584a7a0ca7a4
Paul Butler <paul.butler@ni.com>
2024-09-25 16:16:53 -0500

    add html to GenList.py

--- scripts/GenList.py
+++ scripts/GenList.py
@@ -72,7 +72,7 @@ as noted (a few of our members are songwriters), we make no copyright claim on
 any song.</p>
 """
 
-extensions = [".PDF", ".chopro", ".cho", ".mscz", ".urltxt", ".txt", ".mp3"]
+extensions = [".PDF", ".chopro", ".cho", ".mscz", ".urltxt", ".txt", ".mp3", ".html"]
 allFiles = []
 for p in Path(musicFolder).rglob('*'):
   if ext(p) in (extension.lower() for extension in extensions):
