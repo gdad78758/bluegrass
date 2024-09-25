d75486f92a807bf0f64624158799839e5706f233
Paul Butler <paul.butler@ni.com>
2024-09-25 15:52:00 -0500

    remove confusing near-redundant file name

--- scripts/GenList.py
+++ scripts/GenList.py
@@ -72,7 +72,7 @@ as noted (a few of our members are songwriters), we make no copyright claim on
 any song.</p>
 """
 
-extensions = [".PDF", ".chopro", ".cho", ".mscz", ".urltxt"]
+extensions = [".PDF", ".chopro", ".cho", ".mscz", ".urltxt", ".txt", ".mp3"]
 allFiles = []
 for p in Path(musicFolder).rglob('*'):
   if ext(p) in (extension.lower() for extension in extensions):
